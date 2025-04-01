28fd5f58359e8abcc6606bf6e71086decd1ee433
chemistrying <toto4801110@gmail.com>
2025-04-01 13:15:41 +0800

    Add models

--- flaskr/db/models.py
+++ flaskr/db/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from bson import ObjectId
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class User(BaseModel):
-    _id: ObjectId
+    id: Optional[ObjectId] = Field(alias="_id", default=None)
     email: str
     first_name: str
     last_login: datetime
@@ -15,11 +16,31 @@ class User(BaseModel):
     username: str
 
 
-class UserCreate(BaseModel):
-    email: str
-    first_name: str
-    last_name: str
-    license_key: str
-    major: str
-    password: str
-    username: str
+class Course(BaseModel):
+    id: Optional[ObjectId] = Field(alias="_id", default=None)
+    code: str
+    corequisites: str
+    description: str
+    is_graded: bool
+    not_for_major: str
+    not_for_taken: str
+    prerequisites: str
+    title: str
+    units: str
+
+
+class SemesterPlan(BaseModel):
+    id: Optional[ObjectId] = Field(alias="_id", default=None)
+    course_plan_id: ObjectId
+    courses: list[str]
+    semester: int
+    year: int
+
+
+class CoursePlan(BaseModel):
+    id: Optional[ObjectId] = Field(alias="_id", default=None)
+    description: str
+    favourite: bool
+    name: str
+    updated_at: datetime
+    user_id: ObjectId
