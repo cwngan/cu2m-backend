383bf62759d6126d09c4ba601a42fd630cb24b1b
chemistrying <toto4801110@gmail.com>
2025-04-01 22:51:57 +0800

    Rename get_mongo to get_db

--- flaskr/api/health.py
+++ flaskr/api/health.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_pydantic import validate
 
 from flaskr.api.models import HealthResponseModel
-from flaskr.db.database import get_mongo
+from flaskr.db.database import get_db
 
 route = Blueprint("health", __name__, url_prefix="/health")
 
@@ -10,5 +10,5 @@ route = Blueprint("health", __name__, url_prefix="/health")
 @route.route("/", methods=["GET"])
 @validate()
 def health():
-    db = get_mongo()
+    db = get_db()
     return HealthResponseModel(data={"server": True, "db": db is not None})
