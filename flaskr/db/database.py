9634fa8be876a19f39f32016a7013f126a576afd
chemistrying <toto4801110@gmail.com>
2025-04-01 22:49:46 +0800

    Return database instead of the client itself

--- flaskr/db/database.py
+++ flaskr/db/database.py
@@ -3,9 +3,12 @@ from pymongo import MongoClient
 
 _mongo = None
 
-def get_mongo():
+def get_db():
     global _mongo
     if not _mongo:
         mongo_uri = f"mongodb://{os.getenv("MONGO_DB_USERNAME")}:{os.getenv("MONGO_DB_PASSWORD")}@mongodb:27017/"
+        from dotenv import load_dotenv
+        
+        load_dotenv
         _mongo = MongoClient(mongo_uri)
-    return _mongo
+    return _mongo.database
