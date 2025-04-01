943fc45ee25d5950d7fde80d685517fd3ce02f3d
chemistrying <toto4801110@gmail.com>
2025-04-01 23:56:21 +0800

    Fix load_dotenv problem and add initialize courses to the database

--- flaskr/db/database.py
+++ flaskr/db/database.py
@@ -1,14 +1,31 @@
 import os
+import json
 from pymongo import MongoClient
+from types import SimpleNamespace
+
 
 _mongo = None
+
+
+def init_db():
+    db = get_db()
+    db.courses.drop()
+    
+    db.courses.create_index("code", unique=True)
+    json_courses = json.load(open("req_fmt.json"))
+    for json_course in json_courses.values():
+        json_string = json.dumps(json_course.get("data"))
+        course = json.loads(json_string, object_hook=lambda d: SimpleNamespace(**d))
+        course.original, course.parsed = json_course.get("parsed"), '\n'.join(json_course.get("original"))
+        db.courses.update_one({"code": course.code}, {"$set": course.__dict__}, upsert=True)
+
 
 def get_db():
     global _mongo
     if not _mongo:
-        mongo_uri = f"mongodb://{os.getenv("MONGO_DB_USERNAME")}:{os.getenv("MONGO_DB_PASSWORD")}@mongodb:27017/"
         from dotenv import load_dotenv
         
-        load_dotenv
+        load_dotenv()
+        mongo_uri = f"mongodb://{os.getenv("MONGO_DB_USERNAME")}:{os.getenv("MONGO_DB_PASSWORD")}@mongodb:27017/"
         _mongo = MongoClient(mongo_uri)
     return _mongo.database
