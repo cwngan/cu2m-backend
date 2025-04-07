19d410c79c990114dbfe9e5eb16d17c721ee926f
FieryRMS <raiyanmohdshahrms@gmail.com>
2025-04-07 14:31:50 +0800

    refactor LicenseKeyGenerator variables for clarity, fix classmethod `self` to `cls` in utils.py

--- flaskr/utils.py
+++ flaskr/utils.py
@@ -1,27 +1,29 @@
 import os
-from hashlib import sha256, scrypt
-from base64 import b64encode, b64decode
+from base64 import b64decode, b64encode
+from hashlib import scrypt, sha256
 
-from flaskr.db.models import User, UserCreate
+from flaskr.db.models import UserCreate
 
 
 class LicenseKeyGenerator:
     @classmethod
-    def generate_new_key(self, user_create: UserCreate):
+    def generate_new_key(cls, email: str):
         key, value = os.urandom(24), os.urandom(24)
-        license_key_hash = sha256(user_create.email.encode("ascii") + key + value)
+        license_key_hash = sha256(email.encode("ascii") + key + value)
         return (
             f"{license_key_hash.hexdigest()}.{value.hex()}",
             b64encode(key).decode("ascii"),
         )
 
     @classmethod
-    def verify_key(self, user: User, key: str, license_key: str):
-        hex_hash, value = license_key.split(".")
-        license_key_hash = sha256(
-            user.email.encode("ascii") + key + bytes.fromhex(value)
+    def verify_key(cls, user: UserCreate, license_key_hash: str):
+        expected_license_hash, value = license_key_hash.split(".")
+        user_license_hash = sha256(
+            user.email.encode("ascii")
+            + user.license_key.encode("ascii")
+            + bytes.fromhex(value)
         )
-        return license_key_hash.hexdigest() == hex_hash
+        return user_license_hash.hexdigest() == expected_license_hash
 
 
 class PasswordHasher:
@@ -30,19 +32,19 @@ class PasswordHasher:
     p = 1
 
     @classmethod
-    def __hash_password(self, password: str, salt: bytes, n: int, r: int, p: int):
+    def __hash_password(cls, password: str, salt: bytes, n: int, r: int, p: int):
         return scrypt(password.encode("ascii"), salt=salt, n=n, r=r, p=p)
 
     @classmethod
-    def hash_password(self, password: str):
+    def hash_password(cls, password: str):
         salt = os.urandom(16)
         password_hash = PasswordHasher.__hash_password(
-            password, salt, self.n, self.r, self.p
+            password, salt, cls.n, cls.r, cls.p
         )
         return f"16384$8$1${b64encode(salt).decode('ascii')}${b64encode(password_hash).decode('ascii')}"
 
     @classmethod
-    def verify_password(self, hash: str, password: str):
+    def verify_password(cls, hash: str, password: str):
         n, r, p, salt, password_hash = hash.split("$")
         n, r, p = int(n), int(r), int(p)
         return PasswordHasher.__hash_password(
