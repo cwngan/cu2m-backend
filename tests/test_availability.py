abfff66e4a9a62b3645db14f0b8615848b6fa43d
FieryRMS <raiyanmohdshahrms@gmail.com>
2025-04-08 05:40:38 +0800

    move test fixtures into `conftest.py`, write test for `utils.py`

--- tests/test_availability.py
+++ tests/test_availability.py
@@ -1,58 +1,10 @@
 import re
 from datetime import datetime
 
-import pytest
-from flask import Flask
+from conftest import TEST_DB_NAME
 from flask.testing import FlaskClient
 
-from flaskr import create_app
-from flaskr.db.database import get_mongo_client
 from flaskr.db.user import create_precreated_user, get_precreated_user
-
-# Use a separate database name for testing to avoid clashing with production data.
-TEST_DB_NAME = "TESTDB"
-
-
-@pytest.fixture(autouse=True)
-def setup(monkeypatch: pytest.MonkeyPatch):
-    """
-    Setup testing environment.
-    Monkeypatches the `get_db` function to return a test database.
-
-    This fixture is automatically applied to all tests.
-    """
-
-    def mock_get_db():
-        return get_mongo_client()[TEST_DB_NAME]
-
-    # Note: setattr must be done to all files that imports get_db directly
-    # https://stackoverflow.com/a/45466846
-    monkeypatch.setattr("flaskr.db.database.get_db", mock_get_db)
-    monkeypatch.setattr("flaskr.db.crud.get_db", mock_get_db)
-
-    yield
-    # Clean up the test database after tests
-    get_mongo_client().drop_database(TEST_DB_NAME)
-
-
-@pytest.fixture
-def app():
-    """
-    Create a test Flask app with a test-specific MongoDB URI.
-    """
-    test_config = {
-        "TESTING": True,
-    }
-    app = create_app(test_config)
-    yield app
-
-
-@pytest.fixture
-def client(app: Flask):
-    """
-    Provide a test client for the Flask app.
-    """
-    return app.test_client()
 
 
 def test_api_root(client: FlaskClient):
