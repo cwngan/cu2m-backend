184c5af9ccedad36da59e6e91c2af45e371b8d0a
chemistrying <toto4801110@gmail.com>
2025-04-08 11:08:18 +0800

    Update key generation and verification algorithm to make key prettier

--- flaskr/utils.py
+++ flaskr/utils.py
@@ -1,4 +1,6 @@
 import os
+import string
+import secrets
 from base64 import b64decode, b64encode
 from hashlib import scrypt, sha256
 
@@ -6,23 +8,29 @@ from flaskr.db.models import UserCreate
 
 
 class LicenseKeyGenerator:
+    charset: str = string.digits + string.ascii_uppercase
+
     @classmethod
-    def generate_new_key(cls, email: str):
-        key, value = os.urandom(24), os.urandom(24)
-        license_key_hash = sha256(email.encode("ascii") + key + value)
+    def __generate_subkey(cls, size: int):
+        subkeys = []
+        for i in range(size):
+            subkey = "".join(secrets.choice(cls.charset) for i in range(4))
+            subkeys.append(subkey)
+        return "-".join(subkeys)
+
+    @classmethod
+    def generate_new_key(cls):
+        key, value = cls.__generate_subkey(4), cls.__generate_subkey(2)
+        license_key_hash = sha256(key.encode() + value.encode())
         return (
-            f"{license_key_hash.hexdigest()}.{value.hex()}",
-            b64encode(key).decode("ascii"),
+            f"{license_key_hash.hexdigest()}.{value}",
+            key,
         )
 
     @classmethod
     def verify_key(cls, user: UserCreate, license_key_hash: str):
         expected_license_hash, value = license_key_hash.split(".")
-        user_license_hash = sha256(
-            user.email.encode("ascii")
-            + user.license_key.encode("ascii")
-            + bytes.fromhex(value)
-        )
+        user_license_hash = sha256(user.license_key.encode() + value.encode())
         return user_license_hash.hexdigest() == expected_license_hash
 
 
@@ -32,24 +40,20 @@ class PasswordHasher:
     p = 1
 
     @classmethod
-    def __hash_password(cls, password: str, salt: bytes, n: int, r: int, p: int):
-        return scrypt(password.encode("ascii"), salt=salt, n=n, r=r, p=p)
+    def __hash_password(cls, password: str, salt: str, n: int, r: int, p: int):
+        return scrypt(password.encode("ascii"), salt=b64encode(salt), n=n, r=r, p=p)
 
     @classmethod
     def hash_password(cls, password: str):
         salt = os.urandom(16)
-        password_hash = PasswordHasher.__hash_password(
-            password, salt, cls.n, cls.r, cls.p
-        )
+        password_hash = cls.__hash_password(password, salt, cls.n, cls.r, cls.p)
         return f"16384$8$1${b64encode(salt).decode('ascii')}${b64encode(password_hash).decode('ascii')}"
 
     @classmethod
     def verify_password(cls, hash: str, password: str):
         n, r, p, salt, password_hash = hash.split("$")
         n, r, p = int(n), int(r), int(p)
-        return PasswordHasher.__hash_password(
-            password, b64decode(salt), n, r, p
-        ) == b64decode(password_hash)
+        return cls.__hash_password(password, salt, n, r, p) == b64decode(password_hash)
 
 
 class DataProjection:
