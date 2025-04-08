e25b14012b029c93b9dc68fd9411862bcae3201e
chemistrying <toto4801110@gmail.com>
2025-04-08 18:59:37 +0800

    Fix get_all_courses not working as intended

--- flaskr/db/courses.py
+++ flaskr/db/courses.py
@@ -3,7 +3,7 @@ from flaskr.db.database import get_db
 
 def get_all_courses():
     coursesdb = get_db().courses
-    return coursesdb.courses.find({}).to_list()
+    return coursesdb.find({}).to_list()
 
 
 def get_courses(patterns: list[str]):
