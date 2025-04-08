366923a5b86edbc2d75c54c7ec1c5039acb9c522
chemistrying <toto4801110@gmail.com>
2025-04-08 17:51:51 +0800

    Flush _mongo and remove redundant init_db()

--- flaskr/db/database.py
+++ flaskr/db/database.py
@@ -93,9 +93,9 @@ def get_mongo_client():
         mongo_uri = f"mongodb://{os.getenv('MONGO_DB_USERNAME')}:{os.getenv('MONGO_DB_PASSWORD')}@localhost:27017/"
         print(mongo_uri, flush=True)
         _mongo = MongoClient(mongo_uri)
-        init_db()
+
         print("MongoDB connected", flush=True)
-        print(_mongo)
+        print(_mongo, flush=True)
     return _mongo
 
 
