17f3976caab49dae00e0a6f9c8440cb95eced825
chemistrying <74035480+chemistrying@users.noreply.github.com>
2025-05-02 23:47:23 +0800

    Add support for projection

--- flaskr/db/courses.py
+++ flaskr/db/courses.py
@@ -1,18 +1,19 @@
 from flaskr.db.database import get_db
 
 
-def get_all_courses():
-    coursesdb = get_db().courses
-    return coursesdb.find({}).to_list()
+def get_all_courses(projection: dict[str, bool]):
+    courses_collection = get_db().courses
+    return courses_collection.find({}, projection=projection).to_list()
 
 
-def get_courses(patterns: list[str]):
-    coursesdb = get_db().courses
-    return coursesdb.find(
+def get_courses(patterns: list[str], projection: dict[str, bool]):
+    courses_collection = get_db().courses
+    return courses_collection.find(
         {
             "code": {
                 "$regex": "|".join(["^" + pattern for pattern in patterns]),
                 "$options": "i",
             }
-        }
-    )
+        },
+        projection=projection,
+    ).to_list()
