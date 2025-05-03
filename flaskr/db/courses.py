2db51237bbb1217418f827f333088681e6a9ddb0
chemistrying <74035480+chemistrying@users.noreply.github.com>
2025-05-03 19:04:24 +0800

    Add support for pagination

--- flaskr/db/courses.py
+++ flaskr/db/courses.py
@@ -1,19 +1,25 @@
+from bson import ObjectId
 from flaskr.db.database import get_db
 
 
-def get_all_courses(projection: dict[str, bool]):
+def get_all_courses(projection: dict[str, bool], next: ObjectId, limit: int):
     courses_collection = get_db().courses
-    return courses_collection.find({}, projection=projection).to_list()
+    return courses_collection.find({"_id": {"$gt": next}}, projection=projection).limit(
+        limit
+    )
 
 
-def get_courses(patterns: list[str], projection: dict[str, bool]):
+def get_courses(
+    patterns: list[str], projection: dict[str, bool], next: ObjectId, limit: int
+):
     courses_collection = get_db().courses
     return courses_collection.find(
         {
             "code": {
                 "$regex": "|".join(["^" + pattern for pattern in patterns]),
                 "$options": "i",
-            }
+            },
+            "_id": {"$gt": next},
         },
         projection=projection,
-    ).to_list()
+    ).limit(limit)
