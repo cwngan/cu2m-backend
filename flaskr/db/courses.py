cfbede32ef139b723787af88fd095fd4dfd1d379
chemistrying <74035480+chemistrying@users.noreply.github.com>
2025-05-05 17:12:38 +0800

    Add text search support for get courses function

--- flaskr/db/courses.py
+++ flaskr/db/courses.py
@@ -10,16 +10,43 @@ def get_all_courses(projection: dict[str, bool], after: ObjectId, limit: int):
 
 
 def get_courses(
-    patterns: list[str], projection: dict[str, bool], after: ObjectId, limit: int
+    keywords: list[str], projection: dict[str, bool], after: ObjectId, limit: int
 ):
     courses_collection = get_db().courses
-    return courses_collection.find(
-        {
-            "code": {
-                "$regex": "|".join(["^" + pattern for pattern in patterns]),
-                "$options": "i",
-            },
-            "_id": {"$gt": after},
-        },
-        projection=projection,
-    ).limit(limit)
+    result = []
+    # Search priority: first by code, then by title, then by description
+
+    # Search by code
+    if limit > 0:
+        result += (
+            courses_collection.find(
+                {
+                    "code": {
+                        "$regex": "|".join(["^" + keyword for keyword in keywords]),
+                        "$options": "i",
+                    },
+                    "_id": {"$gt": after},
+                },
+                projection=projection,
+            )
+            .limit(limit)
+            .to_list()
+        )
+    limit -= len(result)
+
+    if limit > 0:
+        result += (
+            courses_collection.find(
+                {
+                    "$text": {
+                        "$search": " ".join(f'"{keyword}"' for keyword in keywords)
+                    }
+                },
+                projection=projection,
+            )
+            .sort({"score": {"$meta": "textScore"}})
+            .limit(limit)
+            .to_list()
+        )
+
+    return result
